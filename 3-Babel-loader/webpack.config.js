95ef761c59dca614744af13362fd00b8be9ed5ff
yellowbin <571704908@qq.com>
2017-04-11 19:41:50 +0800

    1941

--- 3-Babel-loader/webpack.config.js
+++ 3-Babel-loader/webpack.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-	entry : './main.jsx',
-	output :{
-		filename:'bundle.js'
-	},
-	module :{
-		loaders:[
-			{
-				test :/\.js[x]?$/,
-				exclude:/node_modules/,
-				loaders:'babel-loader?presets[]=es2015&presets[]=react'
-			},
-		]
-	}
-}
+  entry: './main.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
